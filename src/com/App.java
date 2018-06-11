4dd8ce75b08a27d09e6a585061843157afb7dda2
user2 <user2@accenture.com>
2018-06-11 14:38:11 +0530

    SOP updated

--- src/com/App.java
+++ src/com/App.java
@@ -4,7 +4,7 @@ public class App {
  public static void main(String[] args) {
 	
 	 System.out.println("hello world");
-	 System.out.println("Hello world : Accenture");
+	 System.out.println("Hello world : Accenture-BDC3");
 	 System.out.println("Hello world : Accenture LKM");
 }
 }
