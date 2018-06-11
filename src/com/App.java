0d6cc49c614012d85d80876432bc85b2a2822151
user1 <user1@accenture.com>
2018-06-11 15:15:26 +0530

    sop deleted

--- src/com/App.java
+++ src/com/App.java
@@ -1,10 +1,11 @@
 package com;
 
 public class App {
- public static void main(String[] args) {
+ public static void main(String[] args)
+ {
 	
 	 System.out.println("hello world");
 	 System.out.println("Hello world : Accenture-IDC");
-	 System.out.println("Hello world : Accenture LKM");
-}
+	
+  }
 }
